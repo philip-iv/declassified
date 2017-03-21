6c37daf6a0d9d9114731f5bb5e4ca454179703d6
LAPTOP-K7QL8DP0\Default <dap332@gmail.com>
2017-03-20 20:05:56 -0400

    Pre loading.

--- client/table.js
+++ client/table.js
@@ -13,11 +13,12 @@
 		$scope.datalod = false;
 		$http.get(url, { cache: true}).success(function(dataFromServer) {
 			// set tableRows
+			$scope.tableRows = dataFromServer;
 			// do any preprocesing
 			$scope.dataLoaded = true;
-			$scope.tableRows = dataFromServer.data;
+			//$scope.tableRows = dataFromServer.data;
 		});
-		$scope.tableRows = [
+/* 		$scope.tableRows = [
 			{"professor": "Augenblick", "rating": 4.8, "tags": "Helpful, Friendly", "course": "CS275", "crn": 12356},
 			{"professor": "Augenblick", "rating": 4.8, "tags": "Helpful, Friendly", "course": "CS265", "crn": 15366},
 			{"professor": "Katsinis", "rating": 3.8, "tags": "Intelligent, Challenging, Cool, Greek ,Friendly", "course": "CS281", "crn": 19856},
@@ -29,11 +30,7 @@
 			{"professor": "Liechty", "rating": 4.2, "tags": "Easy, Dry,  Boring ", "course": "STAT201", "crn": 65844},
 			{"professor": "Schmidt", "rating": 3.4, "tags": "Structured, Strict", "course": "CS265", "crn": 95722},
 			{"professor": "Schmidt", "rating": 3.4, "tags": "Structured, Strict,  ", "course": "CS265", "crn": 95722},
-			
-			
-
-			
-		];
+		]; */
 		
 		$scope.itemsPerPage = 5;
 			
