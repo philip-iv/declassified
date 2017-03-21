77ea20e15b36c9b12faff283ffa00068fc2c5e86
Philip Stephenson <philipstephenson4@gmail.com>
2017-03-20 21:27:38 -0400

    Final commit

--- client/table.js
+++ client/table.js
@@ -16,24 +16,7 @@
 			$scope.tableRows = dataFromServer;
 			// do any preprocesing
 			$scope.dataLoaded = true;
-			//$scope.tableRows = dataFromServer.data;
-		});
-/* 		$scope.tableRows = [
-			{"professor": "Augenblick", "rating": 4.8, "tags": "Helpful, Friendly", "course": "CS275", "crn": 12356},
-			{"professor": "Augenblick", "rating": 4.8, "tags": "Helpful, Friendly", "course": "CS265", "crn": 15366},
-			{"professor": "Katsinis", "rating": 3.8, "tags": "Intelligent, Challenging, Cool, Greek ,Friendly", "course": "CS281", "crn": 19856},
-			{"professor": "Katsinis", "rating": 3.8, "tags": "Intelligent, Challenging, Cool, Greek ,Friendly", "course": "CS283", "crn": 26862},
-			{"professor": "Jodie", "rating": 4.9, "tags": "Easy, Nice, Cool ", "course": "BUSN103", "crn": 98561},
-			{"professor": "Jodie", "rating": 4.9, "tags": "Easy, Nice, Cool ", "course": "BUSN102", "crn": 85988},
-			{"professor": "Liechty", "rating": 4.2, "tags": "Easy, Dry,  Boring ", "course": "STAT201", "crn": 65844},
-			{"professor": "Liechty", "rating": 4.2, "tags": "Easy, Dry,  Boring ", "course": "STAT202", "crn": 65844},
-			{"professor": "Liechty", "rating": 4.2, "tags": "Easy, Dry,  Boring ", "course": "STAT201", "crn": 65844},
-			{"professor": "Schmidt", "rating": 3.4, "tags": "Structured, Strict", "course": "CS265", "crn": 95722},
-			{"professor": "Schmidt", "rating": 3.4, "tags": "Structured, Strict,  ", "course": "CS265", "crn": 95722},
-		]; */
-		
-		$scope.itemsPerPage = 5;
-			
+		});			
 		});
 	
 	
